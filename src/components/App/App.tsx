d89a2a82b0c9aa5cc2d541c0654d7ab50bb2f224
Itay <itay647@gmail.com>
2022-09-20 20:49:58 +0300

    Implement game logic (handleSubmitClick & displayWord)

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,4 @@
-import { useState, ChangeEventHandler, useEffect } from "react";
+import { useState, ChangeEventHandler, useEffect, useMemo } from "react";
 import Misses from "../Misses";
 import "./App.css";
 import HangmanDrawing from "../HangmanDrawing";
@@ -8,10 +8,20 @@ import words from "./words";
 
 function App() {
   const [wordIndex, setWordIndex] = useState<number>();
-  const word = wordIndex !== undefined ? words[wordIndex] : undefined;
+  const word = wordIndex !== undefined ? words[wordIndex] : "";
   const [revealedLetters, setRevealedLetters] = useState<string[]>([]);
-  const [misses, setMisses] = useState<string[]>(["A","B","C"]); // TODO: init as empty
+  const [misses, setMisses] = useState<string[]>([]);
   const [letterInput, setLetterInput] = useState<string>("");
+
+  const createDisplayWord = (word: string, revealedLetters: string[]) => {
+    const displayWord =  word.split("").map(letter => {
+      return revealedLetters.includes(letter) ? letter : "_"
+    }).join(" ");
+
+    return displayWord;
+  }
+
+  const displayWord = useMemo(() => createDisplayWord(word, revealedLetters), [word, revealedLetters]);
 
   useEffect(() => console.log({wordIndex, word}), [wordIndex, word]);
 
@@ -40,8 +50,23 @@ function App() {
     setLetterInput(letter);
   }
 
-  const handleSubmitClick: React.MouseEventHandler<HTMLButtonElement> = () => {
-    
+  const handleSubmitClick = () => {
+    if (letterInput === "") {
+      return;
+    }
+
+    if (revealedLetters.includes(letterInput) || misses.includes(letterInput)) {
+      // NOTE: This is not supposed to happen, but just to make sure:
+      window.alert("You already guessed the letter " + letterInput);
+    } else {
+      console.log("submitted: " + letterInput);
+      if (word?.includes(letterInput)) {
+        setRevealedLetters([...revealedLetters, letterInput]);
+      } else {
+        setMisses([...misses, letterInput]);
+      }
+      setLetterInput("");
+    }
   }
 
   return (
@@ -63,22 +88,27 @@ function App() {
         ))}
       </select>
       <button onClick={() => resetGame()}>Reset game</button>
-      <HangmanDrawing missesCount={misses.length} />
-      {/* TODO: extract to component that gets the (incomplete) word and displays "Word: " with spaces... */}
-      <p>Word: _ _ _ _ { word }</p>
-      <Misses misses={misses} />
-      <input
-        type="text"
-        id="letter-input"
-        name="letter"
-        maxLength={1}
-        placeholder="Enter letter guess"
-        value={letterInput}
-        onChange={handlerLetterInputChange}
-      />
-      {/* TODO: handle submit click */}
-      <button onClick={handleSubmitClick}>Submit</button>
-      {/* TODO: add win/lose display */}
+      {
+        word &&
+        <>
+          <HangmanDrawing missesCount={misses.length} />
+          <p>{"Word: " + displayWord}</p>
+          <Misses misses={misses} />
+          <input
+            type="text"
+            id="letter-input"
+            name="letter"
+            maxLength={1}
+            placeholder="Enter letter guess"
+            value={letterInput}
+            onChange={handlerLetterInputChange}
+            onKeyDown={(e) => {if (e.key === "Enter") handleSubmitClick()}}
+          />
+          <button onClick={handleSubmitClick}>Submit</button>
+          {/* TODO: add win/lose display */}
+        </>
+      }
+      
     </div>
   );
 }
